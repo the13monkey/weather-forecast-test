3169179bc1034cdce5c5b69831606418c31cdbb1
Dinah <dinah.chen.ccy@gmail.com>
2022-03-13 13:33:07 -0700

    set up app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,7 @@ inputForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = inputField.value
     
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 errorField.innerHTML = data.error
